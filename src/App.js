5f98b9002c2b05bec324ef802af21cc39d457347
Bassant <bassantabdalaziz1999@gmail.com>
2023-08-23 04:13:09 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { BrowserRouter , Routes, Route } from 'react-router-dom';
 
 import Home from './component/Home/Home'; // Import your components here
+import Navbar from './component/Navbar/Navbar';
 
 function App() {
   return (
-    <BrowserRouter basename="/React-NewEvent/">
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-
-     </BrowserRouter>
+   <div className='app'>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/'  element={<Home/>} />
+    </Routes>
+    <Home/>
+    </BrowserRouter>
+   </div>
   );
 }
 
